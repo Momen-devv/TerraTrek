15cda3b9c561aa532237c61cf0c1baf73dd9b601
Momen <momen.devv@gmail.com>
2025-06-02 14:19:13 +0300

    Finished Section 9

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const Tour = require('../../models/tourModel');
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<db_password>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose.connect(DB).then(() => console.log('DB connection successful!'));
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'),
+);
+
+const imortData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('done added');
+  } catch (error) {
+    console.log(error);
+  }
+  process.exit();
+};
+
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('done');
+  } catch (error) {
+    console.log(error);
+  }
+  process.exit();
+};
+
+if (process.argv[2] === '--import') {
+  imortData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
